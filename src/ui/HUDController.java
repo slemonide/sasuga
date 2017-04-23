891681a694201a87158103a9397a170e3b6eb0cc
Danil P <slemonide@gmail.com>
2017-04-23 00:43:59 -0700

    Fix Nifty GUI styles and controls dependencies.

---
+++ src/ui/HUDController.java
@@ -0,0 +1,31 @@
+package ui;
+
+import de.lessvoid.nifty.Nifty;
+import de.lessvoid.nifty.screen.Screen;
+import de.lessvoid.nifty.screen.ScreenController;
+
+import javax.annotation.Nonnull;
+
+/**
+ * @author      Danil Platonov <slemonide@gmail.com>
+ * @version     0.1
+ * @since       0.1
+ *
+ * Updates the in-game HUD
+ */
+public class HUDController implements ScreenController {
+    @Override
+    public void bind(@Nonnull Nifty nifty, @Nonnull Screen screen) {
+
+    }
+
+    @Override
+    public void onStartScreen() {
+
+    }
+
+    @Override
+    public void onEndScreen() {
+
+    }
+}
