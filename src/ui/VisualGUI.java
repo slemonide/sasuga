46e723c6aef8c5e15c1cd3f5d040c97267e20256
Danil P <slemonide@gmail.com>
2017-04-22 23:40:47 -0700

    Add a simple HUD.

--- src/ui/VisualGUI.java
+++ src/ui/VisualGUI.java
@@ -2,7 +2,9 @@ package ui;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.math.Vector3f;
+import com.jme3.niftygui.NiftyJmeDisplay;
 import com.jme3.scene.Node;
+import de.lessvoid.nifty.Nifty;
 import jme3tools.optimize.GeometryBatchFactory;
 import model.Cell;
 import model.Position;
@@ -24,6 +26,19 @@ public class VisualGUI extends SimpleApplication {
         eventHandlers.setCursorPosition(new Position());
         environment.initializeEnvironment();
         eventHandlers.initializeEventHandlers();
+        initializeHUD();
+    }
+
+    private void initializeHUD() {
+        NiftyJmeDisplay niftyDisplay = NiftyJmeDisplay.newNiftyJmeDisplay(
+                assetManager, inputManager, audioRenderer, guiViewPort);
+        Nifty nifty = niftyDisplay.getNifty();
+        nifty.fromXml("assets/Interface/hud.xml", "hud");
+        // nifty.fromXml("Interface/helloworld.xml", "start", new MySettingsScreen(data));
+        // attach the Nifty display to the gui view port as a processor
+        guiViewPort.addProcessor(niftyDisplay);
+        // disable the fly cam
+        //flyCam.setDragToRotate(true);
     }
 
     @Override
