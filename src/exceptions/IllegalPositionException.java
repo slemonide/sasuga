2302376d1b01596b3ca718f06258f6f3fc392a62
Danil P <slemonide@gmail.com>
2017-04-26 20:57:04 -0700

    Remove deprecated files (exceptions/*).

--- src/exceptions/IllegalPositionException.java
+++
@@ -1,4 +0,0 @@
-package exceptions;
-
-public class IllegalPositionException extends Exception {
-}
--- src/exceptions/NiftyNotInitializedException.java
+++
@@ -1,7 +0,0 @@
-package exceptions;
-
-/**
- * Thrown when Nifty GUI object is not accessible
- */
-public class NiftyNotInitializedException extends RuntimeException {
-}
