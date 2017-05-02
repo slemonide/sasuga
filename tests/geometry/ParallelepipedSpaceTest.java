cbd391af071f0bb05a89818bd9f4da0417586d7a
Danil P <slemonide@gmail.com>
2017-05-02 01:56:42 -0700

    Add more tests to ParallelepipedSpaceTest.

--- tests/geometry/ParallelepipedSpaceTest.java
+++ tests/geometry/ParallelepipedSpaceTest.java
@@ -11,6 +11,10 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class ParallelepipedSpaceTest {
+    private static final int MAX_POSITIONS = 1000;
+    private static final int MAX_POSITIONS_X = 10;
+    private static final int MAX_POSITIONS_Y = 20;
+    private static final int MAX_POSITIONS_Z = 7;
     private ParallelepipedSpace testSpace;
 
     @Before
@@ -67,6 +71,33 @@ public class ParallelepipedSpaceTest {
     }
 
     @Test
+    public void testAddTwoXReverse() {
+        testSpace.add(new Position(0,0,0));
+        testSpace.add(new Position(1,0,0));
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,0,0),
+                2,1,1)));
+    }
+
+    @Test
+    public void testAddTwoYReverse() {
+        testSpace.add(new Position(0,0,0));
+        testSpace.add(new Position(0,1,0));
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,0,0),
+                1,2,1)));
+    }
+
+    @Test
+    public void testAddTwoZReverse() {
+        testSpace.add(new Position(0,0,0));
+        testSpace.add(new Position(0,0,1));
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,0,0),
+                1,1,2)));
+    }
+
+    @Test
     public void testAddThree() {
         testSpace.add(new Position(1,0,0));
         testSpace.add(new Position(0,0,0));
@@ -75,6 +106,39 @@ public class ParallelepipedSpaceTest {
         assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,0,0),
                 2,1,1)));
         assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(100,0,0))));
+    }
+
+    @Test
+    public void testAddThreeRowStrangeOrder() {
+        testSpace.add(new Position(1,0,0));
+        testSpace.add(new Position(2,0,0));
+        testSpace.add(new Position(0,0,0));
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(1,0,0),
+                3,1,1)));
+    }
+
+    @Test
+    public void testAddThreeRowHarderStrangeOrder() {
+        testSpace.add(new Position(1,0,0));
+        testSpace.add(new Position(2,0,0));
+        testSpace.add(new Position(0,0,0));
+        testSpace.add(new Position(5,0,0));
+        testSpace.add(new Position(4,0,0));
+        testSpace.add(new Position(3,0,0));
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(3,0,0),
+                6,1,1)));
+    }
+
+    @Test
+    public void testAddManyRow() {
+        for (int i = 0; i < MAX_POSITIONS; i++) {
+            testSpace.add(new Position(i, 0, 0));
+        }
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,MAX_POSITIONS/2 - 1,0),
+                MAX_POSITIONS,1,1)));
     }
 
     @Test
@@ -90,5 +154,19 @@ public class ParallelepipedSpaceTest {
         assertEquals(1, testSpace.size());
         assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,0,0),
                 2,2,2)));
+    }
+
+    @Test
+    public void testAddManyParallelepiped() {
+        for (int x = 0; x < MAX_POSITIONS_X; x++) {
+            for (int y = 0; y < MAX_POSITIONS_Y; y++) {
+                for (int z = 0; z < MAX_POSITIONS_Z; z++) {
+                    testSpace.add(new Position(x, y, z));
+                }
+            }
+        }
+        assertEquals(1, testSpace.size());
+        assertTrue(testSpace.getParallelepipeds().contains(new Parallelepiped(new Position(0,MAX_POSITIONS/2 - 1,0),
+                MAX_POSITIONS,1,1)));
     }
 }
