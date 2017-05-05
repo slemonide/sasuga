116215c0bb15e493087c55e3d6df866058e08301
Danil P <slemonide@gmail.com>
2017-05-04 21:16:29 -0700

    Fix Nullpo bug in HUDController. Add cube building animation.

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,4 @@
+import animations.BuildCube;
 import model.*;
 import ui.ConsoleUI;
 import ui.HUDController;
@@ -12,13 +13,16 @@ public class Main {
     public static void main(String[] args) {
         //World.getInstance().addObserver(ConsoleUI.getInstance());
         //World.getInstance().add(new RandomWalkCell(new Position()));
-        for (int x=0; x < SIZE; x++) {
+        /*for (int x=0; x < SIZE; x++) {
             for (int y=0; y < SIZE; y++) {
                 for (int z=0; z < SIZE; z++) {
                     World.getInstance().add(new Cell(new Position(x,y,z)));
                 }
             }
-        }
+        }*/
+
+        Thread buildCubeAnimation = new Thread(new BuildCube());
+        buildCubeAnimation.start();
 
         World.getInstance().start();
         VisualGUI.main(args);
